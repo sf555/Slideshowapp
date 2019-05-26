fe0d4d6d833be97b9e20c0df573bca7e3b4ea8c5
FUKO SUZUKI <test@test-no-MacBook-Pro.local>
2019-05-26 19:07:12 +0900

    new commit

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -13,18 +13,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var returnbtn: UIButton!
     @IBOutlet weak var imageview: UIImageView!
     @IBOutlet weak var slidebtn: UIButton!
+    @IBOutlet var tapgesture: UITapGestureRecognizer!
     
     var flag: Bool = false //スライドショーの実行を判定
 
     var slideshowimage = ["kadaiimage1","kadaiimage2","kadaiimage3"] //画像を格納
     
     @objc func slideshow(_ sender: Timer){ //method of slideshow
-        imageview.image = UIImage(named: self.slideshowimage[dispnum])
         dispnum += 1
         if dispnum >= 3 {
             dispnum = 0 //最後の画像を表示している場合最初の画像に戻す
         }
-        
+        imageview.image = UIImage(named: self.slideshowimage[dispnum])
     }
 
     override func viewDidLoad() {
@@ -64,11 +64,11 @@ class ViewController: UIViewController {
     @IBAction func nextbutton(_ sender: Any) { //進ボタンの設置
         if flag == false {
             
-            imageview.image = UIImage(named: self.slideshowimage[dispnum])
             dispnum += 1
             if dispnum >= 3 {
                 dispnum = 0
             }
+            imageview.image = UIImage(named: self.slideshowimage[dispnum])
             
             }
         }
@@ -76,11 +76,11 @@ class ViewController: UIViewController {
     @IBAction func returnbutton(_ sender: Any) { //戻るボタンの設置
         if flag == false {
             
-            imageview.image = UIImage(named: self.slideshowimage[dispnum])
             dispnum -= 1
             if dispnum <= -1 {
                 dispnum = 2
             }
+            imageview.image = UIImage(named: self.slideshowimage[dispnum])
             }
             
         }
@@ -92,12 +92,14 @@ class ViewController: UIViewController {
             self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(slideshow(_:)), userInfo: nil, repeats: true) // 一定時間ごとにdispnumを１追加
             self.returnbtn.isEnabled = false //進む、戻るボタンを停止
             self.nextBtn.isEnabled = false
+            self.tapgesture.isEnabled = false
             flag = true
             }
         }else if flag == true {
             self.timer.invalidate() // タイマーを停止
             self.returnbtn.isEnabled = true //進む、戻るボタンを停止
             self.nextBtn.isEnabled = true
+            self.tapgesture.isEnabled = true
             flag = false
         }
         
